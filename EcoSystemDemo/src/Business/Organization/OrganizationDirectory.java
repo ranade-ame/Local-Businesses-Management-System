f288d5572db70024e4434fce4000b16af0fbd82a
ranade-ame <ranade.ame@northeastern.edu>
2021-04-13 20:47:50 -0400

    Added plumbing organization to services

--- EcoSystemDemo/src/Business/Organization/OrganizationDirectory.java
+++ EcoSystemDemo/src/Business/Organization/OrganizationDirectory.java
@@ -45,6 +45,10 @@ public class OrganizationDirectory {
             organization = new ElectricianOrganization();
             organizationList.add(organization);
         }
+        else if (type.getValue().equals(Type.Plumbing.getValue())){
+            organization = new PlumbingOrganization();
+            organizationList.add(organization);
+        }
         
         return organization;
     }
