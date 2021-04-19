7c93701f4f81e514b588d329455fccaf7bcc2987
shreyabidarkar <bidarkar.s@northeastern.edu>
2021-04-19 13:01:00 -0400

    Menu added

---
+++ EcosystemDemo/EcoSystemDemo/src/Business/Enterprise/EateriesEnterprise.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author abhishek <your.name at your.org>
+ */
+public class EateriesEnterprise extends Enterprise {
+    public EateriesEnterprise(String name, String streetaddress, String city, String country, int zipcode, String email, String phone){
+        super(name,EnterpriseType.Eateries, streetaddress, city, country, zipcode, email, phone);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
---
+++ EcosystemDemo/EcoSystemDemo/src/Business/Organization/MechanicOrganization.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.MechanicRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ameya
+ */
+public class MechanicOrganization extends Organization{
+
+    public MechanicOrganization(String name, String streetaddress, String city, String country, int zipcode, String email, String phone) {
+        super(Organization.Type.Mechanic.getValue(), name, streetaddress, city, country, zipcode, email, phone);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new MechanicRole());
+        return roles;
+    }
+    
+}
