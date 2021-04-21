391c7447929d6314aeb91197098efb1e9f0304f9
ranade-ame <ranade.ame@northeastern.edu>
2021-04-21 16:39:07 -0400

    Transport and Delivery implemented

---
+++ EcoSytemDemo/EcoSystemDemo/src/Business/Enterprise/ServiceEnterprise.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ameya
+ */
+public class ServiceEnterprise extends Enterprise{
+
+    public ServiceEnterprise(String name, String streetaddress, String city, String country, int zipcode, String email, String phone) {
+        super(name,EnterpriseType.Service, streetaddress, city, country, zipcode, email, phone);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        return null;
+    }
+    
+}
---
+++ EcoSytemDemo/EcoSystemDemo/src/Business/Organization/LabOrganization.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Organization;
+
+import Business.Role.LabAssistantRole;
+import Business.Role.Role;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author raunak
+ */
+public class LabOrganization extends Organization{
+
+    public LabOrganization(String name, String streetaddress, String city, String country, int zipcode, String email, String phone) {
+        super(Organization.Type.Lab.getValue(), name, streetaddress, city, country, zipcode, email, phone);
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new LabAssistantRole());
+        return roles;
+    }
+     
+   
+    
+    
+}
